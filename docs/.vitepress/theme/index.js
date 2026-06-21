9908a2878885b11a45b535135b76a8dd6febc8dd
Stephen Collins <stephen@stephencollins.tech>
2026-06-21 12:08:42 -0500

    docs: add VitePress theme files

---
+++ docs/.vitepress/theme/index.js
@@ -0,0 +1,7 @@
+import DefaultTheme from 'vitepress/theme'
+import './custom.css' // This safely registers your custom styles
+
+export default {
+  extends: DefaultTheme,
+  // You can also register custom layout components here if needed
+}
